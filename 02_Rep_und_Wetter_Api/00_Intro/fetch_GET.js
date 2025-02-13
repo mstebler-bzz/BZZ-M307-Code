c97cc380eb04bce5ee1f58a50dc70239db9785e7
M. Stebler <martin.stebler@bzz.ch>
2025-02-13 10:03:52 +0100

    adding intro and exercices for 02

---
+++ 02_Rep_und_Wetter_Api/00_Intro/fetch_GET.js
@@ -0,0 +1,2 @@
+// Laden Sie alle Posts von https://jsonplaceholder.typicode.com/posts und geben Sie sie auf der Konsole aus
+// https://jsonplaceholder.typicode.com/posts
---
+++ 02_Rep_und_Wetter_Api/00_Intro/fetch_POST.js
@@ -0,0 +1,2 @@
+// Senden Sie einen POST-Request an https://jsonplaceholder.typicode.com/posts und geben Sie Ihren Namen sowie ihre Lieblingspizza mit
+// https://jsonplaceholder.typicode.com/posts
